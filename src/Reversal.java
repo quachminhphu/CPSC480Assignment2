0b24efacd4f89bb9d237265802437247c6ea5a2a
quachminhphu <phu.quach.13@cnu.edu>
2015-10-09 16:35:20 -0400

    Create method

--- src/Reversal.java
+++ src/Reversal.java
@@ -1,4 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Stack;
+
 
 public class Reversal {
-
+	
+	/*
+	 * This method receives input file and put word in container 
+	 */
+	public static void readFile (File input ,Stack<String> Container){
+		
+	}
+	
+	/*
+	 * This method receives container and write it into file in reverse order 
+	 */
+	public static void writeFile (File output, Stack<String> Container){
+		
+	}
+	
+	/*
+	 * The method receives input file and output file 
+	 * Reads lines of text from the input file and writes these lines in reverse order 
+	 */
+	public static void reverseFile(File input, File output) throws FileNotFoundException {
+		
+	}
 }
