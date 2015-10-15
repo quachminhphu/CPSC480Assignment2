a2c3274230b5ceedd54e8c32adf635160b0e0d58
quachminhphu <phu.quach.13@cnu.edu>
2015-10-15 18:20:17 -0400

    Implementing method

--- src/Reversal.java
+++ src/Reversal.java
@@ -1,5 +1,8 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 import java.util.Stack;
 
 
@@ -8,7 +11,27 @@ public class Reversal {
 	/*
 	 * This method receives input file and put word in container 
 	 */
-	public static void readFile (File input ,Stack<String> Container){
+	public static void readFile (File input ,Stack<String> container) throws FileNotFoundException{
+		Scanner reader = new Scanner(input);
+		while (reader.hasNextLine()){
+			// Start a new line 
+			container.add(new String("\0"));
+			
+			String line = reader.nextLine();
+			Scanner lineProcessor = new Scanner(line);
+			
+			//Scan every word in the line 
+			while (lineProcessor.hasNext()){
+				String word = lineProcessor.next();
+				container.push(word);
+			}
+			//finish a line 
+			lineProcessor.close();
+		}
+		//Finish with ending character
+		container.add(new String("\0"));
+		
+		reader.close();
 		
 	}
 	
@@ -16,7 +39,39 @@ public class Reversal {
 	 * This method receives container and write it into file in reverse order 
 	 */
 	public static void writeFile (File output, Stack<String> Container){
+		try {
+		PrintWriter printer = new PrintWriter(output);
+		String line="";
+		String temp ="";
+		String mynull ="\0";
 		
+		while (!Container.isEmpty()){
+			temp = Container.pop();
+			
+			if(temp.equals(mynull)){
+				if(line.isEmpty()){
+					continue;
+				}else {
+					printer.println(line);
+					line ="";
+				}
+			}else if( temp.equals(" ")){
+				continue;
+			}else {
+				if(line.isEmpty()){
+					line = line + temp;
+				}else {
+					line = line+ " "+temp;
+				}
+			}
+			
+			
+		}
+		printer.close();
+		
+		}catch(IOException ex){
+			
+		}
 	}
 	
 	/*
@@ -24,6 +79,8 @@ public class Reversal {
 	 * Reads lines of text from the input file and writes these lines in reverse order 
 	 */
 	public static void reverseFile(File input, File output) throws FileNotFoundException {
-		
+		Stack<String> container = new Stack<String>();
+		Reversal.readFile(input, container);
+		Reversal.writeFile(output, container);
 	}
 }
